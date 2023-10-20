59a89c0a80e9f24009b7f8b759a599dc94311aa3
Viniciu <contato.viniciusdanielmonteiro@gmail.com>
2023-10-19 22:16:42 -0400

    Descrição de evento

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,27 +1,29 @@
 import Link from 'next/link'
 import styles from './styles.module.css'
 import Button from '../Button'
-export default function Card({id, imagem, titulo, descricao, dataInicio, dataFim, local }) {
+export default function Card({ id, imagem, titulo, descricao, dataInicio, dataFim, local }) {
 
     return (
         <>
-            <Link alt='Saiba mais' href={`/eventos/${id}`}>
-                <div className={styles.card}>               
+            <div>
+                <div className={styles.card}>
                     <div>
-                    <h2 className={styles.cardTitulo}>{titulo}</h2>
-                    <img className={styles.img} src={imagem}/>
-                    
-                    <div className={styles.subcontainer}>Descrição: {descricao}</div>
-                    <br/>
-                    <div>Data de Início: {dataInicio}</div>
-                    <br/>
-                    <div>Data de Fim: {dataFim}</div>
-                    <br/>
-                    <div>Local: <br/> {local}</div>
+                        <img className={styles.img} src={imagem} />
+                        <h2 className={styles.cardTitulo}>{titulo}</h2>
+                        <div className={styles.subcontainer}>Descrição: {descricao}</div>
+                        <br />
+                        <div>Data de Início: {dataInicio}</div>
+                        <br />
+                        <div>Data de Fim: {dataFim}</div>
+                        <br />
+                        <div>Local: <br /> {local}</div>
+                    <Link href={`/eventos/${id}`}>
+                       <Button className={styles.button}>Saiba mais</Button>
+                    </Link>
+                     
                     </div>
-        
                 </div>
-            </Link>
+            </div>
         </>
     )
 }
--- src/pages/eventos/[id].js
+++ src/pages/eventos/[id].js
@@ -1,9 +1,10 @@
+import styles from './styles.module.css'
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useEffect, useState } from "react";
 export default function EventosHome() {
-    
-    const [evento ,setEvento] = useState ({})
+
+    const [evento, setEvento] = useState({})
     const router = useRouter()
 
     useEffect(() => {
@@ -11,16 +12,30 @@ export default function EventosHome() {
         const id = router.query.id
         if (id) {
             axios.get(`http://localhost:3000/eventos/${id}`)
-            .then(resultado => setEvento(resultado.data))
+                .then(resultado => setEvento(resultado.data))
         }
     }, [])
 
 
-return(
-    <>
-    <h1>Detalhe do Evento</h1>
-    <p>{evento.titulo}</p>
-    <p>{evento.descricao}</p>
-    </>
-)
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.titulo}>
+                    <img
+                        style=
+                        {{ borderRadius: '10px', width: '1000px' }}
+                        src={evento.imagem} />
+
+                    <div className={styles.subcontainer}>
+                        <h1>{evento.titulo}</h1>
+                        <p>Descrição: <br />
+                            {evento.descricao}</p>
+                        <br />
+                        <p>Data de Inicio: {evento.dataInicio} | <br /> Data de Fim:{evento.dataFim}</p>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
